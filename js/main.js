a025a7576884682ceb9413c7a1361ff849236f54
Daniil <ipshenichnyy@inbox.ru>
2022-05-17 21:55:51 +0300

    desktop-last

--- js/main.js
+++ js/main.js
@@ -27,4 +27,48 @@ window.addEventListener("load", function () {
             clickable: true,
         },
     });
+
+});
+
+window.addEventListener("load", function () {
+    let hamburger = document.querySelector('.v-search');
+
+    const toggleMenu = () => {
+        hamburger.classList.toggle('active');
+    }
+
+    hamburger.addEventListener('click', e => {
+        e.stopPropagation();
+
+        toggleMenu();
+    });
+});
+
+
+window.addEventListener("load", function () {
+    let hamburger1 = document.querySelector('.h-link-order');
+
+    const toggleMenu = () => {
+        hamburger1.classList.toggle('active');
+    }
+
+    hamburger1.addEventListener('click', e => {
+        e.stopPropagation();
+
+        toggleMenu();
+    });
+});
+
+window.addEventListener("load", function () {
+    let hamburger2 = document.querySelector('.add-sale');
+
+    const toggleMenu = () => {
+        hamburger2.classList.toggle('active');
+    }
+
+    hamburger2.addEventListener('click', e => {
+        e.stopPropagation();
+
+        toggleMenu();
+    });
 });
